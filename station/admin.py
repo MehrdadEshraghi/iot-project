a17dbb915fce32385048ed2f01d1b256fa689399
Mehrdad Eshraghi <mehrdad4502@yahoo.com>
2023-05-23 23:16:05 +0330

    finish models for now

--- station/admin.py
+++ station/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import *
+
+admin.site.register(Station)
+admin.site.register(Module)
+admin.site.register(Sensor)
+admin.site.register(Log)
--- station/models.py
+++ station/models.py
@@ -3,17 +3,17 @@ from django.db import models
 # Create your models here.
 
 class Sensor(models.Model):
-    type = models.CharField(max_length=30)
+    type = models.CharField(max_length=10, choices=[('one', 'two'), ('three', 'four')])
     model = models.CharField(max_length=30)
     sampling_rate = models.CharField(max_length=30)
     operational_capacity = models.DecimalField(decimal_places=3, max_digits=7)
-    module = models.ForeignKey('Module', on_delete=models.CASCADE)
-    # logs = 
+    module = models.ForeignKey('Module', blank=True, null=True,on_delete=models.CASCADE)
+    # logs = models.ForeignKey('Log', on_delete=models.CASCADE, related_name='+', blank=True, null=True)
 
 class Module(models.Model):
     mobile_number = models.CharField(max_length=30)
     port = models.CharField(max_length=10)
-    protocol_type = models.CharField(max_length=10, choices=[('this', 'that'), ('that', 'that')])
+    protocol_type = models.CharField(max_length=10, choices=[('one', 'two'), ('three', 'four')])
     station = models.ForeignKey('Station', on_delete=models.CASCADE)
     sensors = models.ForeignKey(Sensor, on_delete=models.CASCADE, related_name='+')
 
@@ -24,4 +24,12 @@ class Station(models.Model):
     name = models.CharField(max_length=30)
     nominal_capacity = models.DecimalField(decimal_places=3, max_digits=7)
     operational_capacity = models.DecimalField(decimal_places=3, max_digits=7)
-    modules = models.ForeignKey(Module, on_delete=models.CASCADE, related_name='+')
+    modules = models.ForeignKey(Module, on_delete=models.CASCADE, related_name='+', blank=True, null=True)
+
+class Log(models.Model):
+    modules = models.ForeignKey(Module, on_delete=models.CASCADE)
+    value = models.DecimalField(decimal_places=3, max_digits=7)
+    unit = models.CharField(max_length=10, choices=[('one', 'two'), ('three', 'four')])
+    type = models.CharField(max_length=10, choices=[('one', 'two'), ('three', 'four')])
+    date = models.DateField()
+    time = models.TimeField()
